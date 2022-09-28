1a712115dede248debba30d4149e59b7f9514e38
GitHub Action <action@github.com>
2022-09-28 09:12:28 +0000

    Template cleanup

--- src/main/kotlin/com/github/cloudyant/ijplugintemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/cloudyant/ijplugintemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.cloudyant.ijplugintemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.cloudyant.ijplugintemplate.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/cloudyant/ijplugintemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/cloudyant/ijplugintemplate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.cloudyant.ijplugintemplate.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.cloudyant.ijplugintemplate.MyBundle
 
 class MyProjectService(project: Project) {
 
